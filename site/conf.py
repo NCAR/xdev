89f957cbdf18c5b43a21c9e55b822b1b5734dd54
Kevin Paul <kpaul@ucar.edu>
2020-02-11 05:54:20 -0700

    Add Google Analytics to base template

--- site/conf.py
+++ site/conf.py
@@ -1204,7 +1204,17 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+# BODY_END = """
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-158200162-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-158200162-1');
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
