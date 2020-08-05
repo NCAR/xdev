4c432b60ad47ba8108d67f44f60688cf28555a06
Kevin Paul <kpaul@ucar.edu>
2020-08-05 14:36:19 -0600

    Another empty repos list fix

--- .github/workflows/register_repositories.py
+++ .github/workflows/register_repositories.py
@@ -57,7 +57,9 @@ def parse_line(line, original_config, repos={'remove': [], 'add': []}):
 
         if valid:
             if split_on == add_cmd:
-                if config[info['campaign']]['repos'] is None or info['repo'] not in set(config[info['campaign']]['repos']):
+                if config[info['campaign']]['repos'] is None:
+                    config[info['campaign']]['repos'] = []
+                if info['repo'] not in set(config[info['campaign']]['repos']):
                     config[info['campaign']]['repos'].append(info['repo'])
                     repos['add'].append(info['repo'])
 
